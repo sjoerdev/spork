a82a9183aacc436c9088be854673345a76dd8b71
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2024-11-19 20:48:40 +0100

    intigrate input into main engine class

--- src/engine/Engine.cs
+++ src/engine/Engine.cs
@@ -49,8 +49,20 @@ public class Engine
     public int windowHeight = 600;
     public Color clearColor = Color.Black;
 
+    // input
+    private List<Keys> keysPressed = [];
+    private List<Keys> keysDown = [];
+    private List<Keys> keysUp = [];
+    private List<MouseButtons> mouseButtonsPressed = [];
+    private List<MouseButtons> mouseButtonsDown = [];
+    private List<MouseButtons> mouseButtonsUp = [];
+    private List<Keys> keysDownLastFrame = [];
+    private List<Keys> keysUpLastFrame = [];
+    private List<MouseButtons> mouseButtonsDownLastFrame = [];
+    private List<MouseButtons> mouseButtonsUpLastFrame = [];
+    private Vector2 mousePosition = Vector2.Zero;
+
     // other
-    public Input input;
     public float deltaTime = 0.0f;
     public float angle = 0.0f;
     public Vector2 scale = Vector2.One;
@@ -72,8 +84,8 @@ public class Engine
 
         InitializeDeviceResources();
         InitializeAudioStuff();
-
-        input = new(renderForm);
+        InitializeInputListening();
+        
         currentBrush = new(renderTarget, SharpDX.Color.Black);
         defaultFont = new("Arial", 12.0f);
     }
@@ -113,6 +125,15 @@ public class Engine
         outputDevice.Play();
     }
 
+    private void InitializeInputListening()
+    {
+        renderForm.KeyDown += (o, e) => keysDownLastFrame.Add(e.KeyCode);
+        renderForm.KeyUp += (o, e) => keysUpLastFrame.Add(e.KeyCode);
+        renderForm.MouseDown += (o, e) => mouseButtonsDownLastFrame.Add(e.Button);
+        renderForm.MouseUp += (o, e) => mouseButtonsUpLastFrame.Add(e.Button);
+        renderForm.MouseMove += (o, e) => mousePosition = new(e.X, e.Y);
+    }
+
     public void Run()
     {
         UpdateSubscriptions();
@@ -126,7 +147,8 @@ public class Engine
         {
             renderTarget.BeginDraw();
             renderTarget.Clear(new RawColor4(clearColor.R, clearColor.G, clearColor.B, clearColor.A));
-            input.Update();
+            HandleKeyboardInput();
+            HandleMouseInput();
             foreach (GameObject go in gameObjects) go.Update();
             canpaint = true;
             foreach (GameObject go in gameObjects) go.Paint();
@@ -145,6 +167,78 @@ public class Engine
         foreach (GameObject go in unsubscribingGameObjects) gameObjects.Remove(go);
         subscribingGameObjects.Clear();
         unsubscribingGameObjects.Clear();
+    }
+
+    private void HandleKeyboardInput()
+    {
+        keysDown.Clear();
+        keysUp.Clear();
+
+        foreach (Keys key in keysDownLastFrame)
+        {
+            if (keysPressed.Contains(key) == false)
+            {
+                keysDown.Add(key);
+                keysPressed.Add(key);
+            }
+        }
+
+        foreach (Keys key in keysUpLastFrame)
+        {
+            if (keysPressed.Contains(key) == true)
+            {
+                keysUp.Add(key);
+                keysPressed.Remove(key);
+            }
+        }
+
+        keysDownLastFrame.Clear();
+        keysUpLastFrame.Clear();
+    }
+
+    private void HandleMouseInput()
+    {
+        mouseButtonsDown.Clear();
+        mouseButtonsUp.Clear();
+
+        foreach (MouseButtons button in mouseButtonsDownLastFrame)
+        {
+            if (mouseButtonsPressed.Contains(button) == false)
+            {
+                mouseButtonsDown.Add(button);
+                mouseButtonsPressed.Add(button);
+            }
+        }
+
+        foreach (MouseButtons button in mouseButtonsUpLastFrame)
+        {
+            if (mouseButtonsPressed.Contains(button) == true)
+            {
+                mouseButtonsUp.Add(button);
+                mouseButtonsPressed.Remove(button);
+            }
+        }
+
+        mouseButtonsDownLastFrame.Clear();
+        mouseButtonsUpLastFrame.Clear();
+    }
+
+    public bool GetKey(Keys key) => keysPressed.Contains(key);
+    public bool GetKeyDown(Keys key) => keysDown.Contains(key);
+    public bool GetKeyUp(Keys key) => keysUp.Contains(key);
+    public bool GetMouseButton(int buttonID) => mouseButtonsPressed.Contains(TranslateMouseButton(buttonID));
+    public bool GetMouseButtonDown(int buttonID) => mouseButtonsDown.Contains(TranslateMouseButton(buttonID));
+    public bool GetMouseButtonUp(int buttonID) => mouseButtonsUp.Contains(TranslateMouseButton(buttonID));
+    public Vector2 GetMousePosition() => mousePosition;
+    private MouseButtons TranslateMouseButton(int buttonID)
+    {
+        return buttonID switch
+        {
+            0 => MouseButtons.Left,
+            1 => MouseButtons.Right,
+            2 => MouseButtons.Middle,
+            _ => MouseButtons.None,
+        };
     }
 
     public void PlayAudio(AudioClip clip) => mixer.AddMixerInput(ConvertToRightChannelCount(clip));
